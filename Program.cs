a0fa1c6ed76025f02d976c18f6ad048ea643a694
Adela Kolawa <Adela Kolawa>
2016-05-16 10:23:07 +0200

    Only tests

--- Program.cs
+++ Program.cs
@@ -10,13 +10,22 @@ namespace KolawaAdela
 {
     class Program
     {
-       
+       //<summary>Array numbers of semiprimes within specified ranges.</summary>
+       //<param name="N">The most semiprime in array. </param>
+       //<param name="P">Array first number of specified range.</param>
+       //<param name="M">Array last number of specified range.</param>
+       //<returns>Array queries about the number of semiprimes within specified ranges</returns>
+
         public static int[] NumberSemiprimes(int N, int[] P, int []Q, int M)
         {
+
+
             return null;
+           
         }
         static void Main(string[] args)
         {
+          
             Console.ReadKey();
         }
     }
@@ -32,7 +41,7 @@ namespace KolawaAdela
             int[] R = new int[] { 10, 4, 0 };
             for(int i=0; i<R.Length;i++)
             {
-                Assert.AreEqual(R[i],Program.NumberSemiprimes(26,P,Q,3));
+                Assert.AreEqual(R[i],Program.NumberSemiprimes(26,P,Q,3)[i]);
             }
 
           
